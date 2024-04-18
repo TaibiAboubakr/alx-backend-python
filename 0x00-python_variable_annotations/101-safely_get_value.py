046eeebf8942744d960c82aa978cecffd49e7d89
aboubakr <c.aboubakr@gmail.com>
2024-04-18 15:31:05 +0100

    add the file 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" takes a sequance and return first element"""
+from typing import Sequence, Union, Any, Mapping, TypeVar
+
+
+T = TypeVar('T')
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """ takes a sequance and return first element"""
+
+    if key in dct:
+        return dct[key]
+    else:
+        return default
