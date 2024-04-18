b03236867bb4155c89e79298fcfef64c853b5676
aboubakr <c.aboubakr@gmail.com>
2024-04-18 15:52:46 +0100

    update the file 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-from typing import Tuple, List, Union
+""" takes two arguments tuple and int
+and returs a list multiple of tuple """
+from typing import Tuple, List
+
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ takes two arguments tuple and int
+and returs a list multiple of tuple """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
@@ -13,3 +19,4 @@ array = (12, 72, 91)
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
+print(zoom_2x)
