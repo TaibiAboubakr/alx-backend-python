f1ff78dcc2594eea136176877affb6ca0fff0df2
aboubakr <c.aboubakr@gmail.com>
2024-04-18 13:06:27 +0100

    add the file 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""  takes a float n as argument and
+returns the string representation of the float """
+import math
+
+
+def to_str(n: float) -> str:
+    """  takes a float n as argument and
+    returns the string representation of the float """
+    return n
